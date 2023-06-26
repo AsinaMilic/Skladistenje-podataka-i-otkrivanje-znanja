616026e35db61881cc25c5a3d387c8b9ba9332ad
Aleksa Milic <aleksamilic2224@gmail.com>
2023-06-26 19:35:25 +0200

    printing trees left?

--- projekat 3/proj3.py
+++ projekat 3/proj3.py
@@ -1,21 +1,29 @@
 from pyspark.ml.classification import LinearSVC, DecisionTreeClassifier, LogisticRegression, NaiveBayes, GBTClassifier
 from pyspark.ml.evaluation import BinaryClassificationEvaluator, MulticlassClassificationEvaluator
 from pyspark.ml.feature import StringIndexer, VectorAssembler, MinMaxScaler
+from pyspark.ml.stat import Correlation
+from pyspark.ml.linalg import DenseMatrix, Vectors
 from pyspark.ml import Pipeline
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col,sum,stddev,stddev_samp,expr,when, udf
+from pyspark.sql.functions import col, sum, stddev, stddev_samp, expr, when, udf, mean 
 from pyspark.sql import functions as F
 from pyspark.sql.types import DoubleType
 from pyspark.mllib.evaluation import MulticlassMetrics
+#from pyspark.ml.stat import ChiSquareTest
 import sys
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from pyspark.sql.functions import col as spark_col
 
-
-PREPROCESS_DATA = True
-columns = ["Area", "MajorAxisLength", "MinorAxisLength", "Eccentricity", "ConvexArea","EquivDiameter", "Extent", "Perimeter", "Roundness", "AspectRation"]
-columns_scaled = ["Area_S", "MajorAxisLength_S", "MinorAxisLength_S", "Eccentricity_S", "ConvexArea_S","EquivDiameter_S", "Extent_S", "Perimeter_S", "Roundness_S", "AspectRation_S"]
+PREPROCESS_DATA = False
+columns = ["Area", "MajorAxisLength", "MinorAxisLength", "Eccentricity", "ConvexArea", "EquivDiameter", "Extent", "Perimeter", "Roundness", "AspectRation"]
+columns_scaled = ["Area_S", "MajorAxisLength_S", "MinorAxisLength_S", "Eccentricity_S", "ConvexArea_S", "EquivDiameter_S", "Extent_S", "Perimeter_S", "Roundness_S", "AspectRation_S"]
 
 
 def classificator_evaluation(PredictionAndLabels, output_file):
+    if(PREPROCESS_DATA):
+        output_file = "Preprocessed " +output_file 
     Metrics = MulticlassMetrics(PredictionAndLabels)
     ConfusionMatrix = Metrics.confusionMatrix()
     cm = ConfusionMatrix.toArray()
@@ -27,21 +35,21 @@ def classificator_evaluation(PredictionAndLabels, output_file):
     N = FN + TN
     Ukupno = P + N
 
-    accuracy = (TP + TN) / cm.sum() #Ukupno
+    accuracy = (TP + TN) / cm.sum()  # Ukupno
     error_rate = 1 - accuracy
-    sensitivity = TP/P #(TP) / (TP + FN)
-    specificity = TN/N #(TN) / (TN + FP)
+    sensitivity = TP / P  # (TP) / (TP + FN)
+    specificity = TN / N  # (TN) / (TN + FP)
     precision = (TP) / (TP + FP)
-    recall = TP/P
-    F = (2*precision*recall) / (precision+recall)
-    pe = (TP+FP)*(TP+FN)/N*N + (TN+FN)*(TN+FP)/N*N  #kappa 
+    recall = TP / P
+    F = (2 * precision * recall) / (precision + recall)
+    pe = (TP + FP) * (TP + FN) / N * N + (TN + FN) * (TN + FP) / N * N  # kappa
 
     with open(output_file, 'w') as file:
         sys.stdout = file
         print("                 Rezultat klasifikacije           ")
         print("                      Da       Ne           Ukupno")
         print(f"Poznata    Da        {TP}    {FN}           {P}")
-        print(f" klasa     Ne         {FP}    {TN}          {N}")
+        print(f" klasa     Ne         {FP}     {TN}          {N}")
         print(f"         Ukupno      {P}    {N}          {Ukupno}")
         print("\n\n\n")
         print("Number of instances: ", Ukupno)
@@ -65,8 +73,8 @@ def preprocess_data(data):
     print("\nID kolona uklonjena::")
     data.show()
 
-    # Izbroj i izbaci iste redove (imaju sve vrednosti atributa iste)
-    duplicate_count = data.dropDuplicates().count() - data.count()
+    # Count and remove iste redove (imaju sve vrednosti atributa iste)
+    duplicate_count = data.count() - data.dropDuplicates().count()  
     print(f"\nNumber of duplicate rows: {duplicate_count}\n")
 
     print("\nKoliko polja u svakoj koloni su null?")
@@ -75,12 +83,13 @@ def preprocess_data(data):
     print("\nDa li vrednosti atributa class balansirane?")
     data.groupBy("class").count().show()
 
-    # Eliminisemo "outliers" vrednosti za ove kolone
-    statistics = data.select([stddev_samp(column).alias(column) for column in columns]).first() # Calculate the mean and standard deviation for each column
+    # Eliminisemo "outliers" vrednosti za ove kolone. Calculate the mean and standard deviation for each column
+    statistics = data.select([stddev_samp(column).alias(column) for column in columns]).first()  
+    mean_values = data.select([mean(column).alias(column) for column in columns]).first()
 
     # Define the lower and upper bounds for outliers (moze i 3)
-    lower_bounds = [(statistics[column] - 2 * statistics[column]) for column in columns]
-    upper_bounds = [(statistics[column] + 2 * statistics[column]) for column in columns]
+    lower_bounds = [(mean_values[column] - 2 * statistics[column]) for column in columns]
+    upper_bounds = [(mean_values[column] + 2 * statistics[column]) for column in columns]
 
     # Filter out rows with values outside the bounds
     print("\nFiltrirani podaci:")
@@ -88,50 +97,74 @@ def preprocess_data(data):
 
     print("\nNormalizovani podaci:")
     # UDF for converting column type from vector to double type
-    unlist = udf(lambda x: round(float(list(x)[0]),15), DoubleType())
+    unlist = udf(lambda x: round(float(list(x)[0]), 15), DoubleType())
     for i in columns:
-        assembler = VectorAssembler(inputCols=[i],outputCol=i+"_Vect") # VectorAssembler Transformation - Converting column to vector type
-        scaler = MinMaxScaler(inputCol=i+"_Vect", outputCol=i+"_S") # MinMaxScaler Transformation
+        assembler = VectorAssembler(inputCols=[i],outputCol=i + "_Vect")  # VectorAssembler Transformation - Converting column to vector type
+        scaler = MinMaxScaler(inputCol=i + "_Vect", outputCol=i + "_S")  # MinMaxScaler Transformation
         pipeline = Pipeline(stages=[assembler, scaler])  # Pipeline of VectorAssembler and MinMaxScaler
-        data = pipeline.fit(data).transform(data).withColumn(i+"_S", unlist(i+"_S")).drop(i+"_Vect")  # Fitting pipeline on dataframe
+        data = pipeline.fit(data).transform(data).withColumn(i + "_S", unlist(i + "_S")).drop(i + "_Vect")  # Fitting pipeline on dataframe
 
-    # Drop the first 10 columns
     drop_columns = data.columns[:10]
     data = data.drop(*drop_columns)
-    data = data.select(*([col(c) for c in data.columns[1:]] + [col(data.columns[0])])) # Move the 11th column to the end
+    data = data.select(*([col(c) for c in data.columns[1:]] + [col(data.columns[0])]))  # Move the 11th column to the end
     data.show()
 
-    return data   
+    assembledData = convert_to_ass(data)
+    correlation(assembledData)
+    correlation_heatmap(assembledData,1)
+    columns_scaled.remove("ConvexArea_S")
+    columns_scaled.remove("EquivDiameter_S")
+    assembledData = convert_to_ass(data) 
+    correlation_heatmap(assembledData,2)
+
+    return assembledData
 
 
-if __name__ == "__main__":
-    spark = SparkSession.builder \
-        .appName("Rice Classificator") \
-        .getOrCreate()
-    spark.sparkContext.setLogLevel("WARN") # ne zelim da mi se INFO logovi prikazuju u konzoli
-    path = "./Rice.csv"
-
-    ###########################  1) Učitavanje podataka  #######################################################
-    raw_data = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(path)
+def correlation(assembledData):
+    for col in columns_scaled: 
+        if col not in ["Class", "label"]: 
+            correlation = assembledData.corr("Class", col) # Izračunavajte Pearsonovu korelaciju između kolone "Class" i trenutne kolone
+            print(f"Pearson correlation for Class with {col} = {correlation}")
 
 
-    ########################  2) Preprocesranje (koristeci spark) ##############################################
-    if (PREPROCESS_DATA):
-        data = preprocess_data(raw_data)
-     #eventualno da koristim selectovane attribute
+def correlation_heatmap(assembledData, iter):
+    matrix = Correlation.corr(assembledData, "features").head()[0] # Get correlation matrix
 
+    corr_matrix = matrix.toArray()
+    corr_pd = pd.DataFrame(corr_matrix, columns=columns_scaled, index=columns_scaled) # Convert correlation matrix to Pandas DataFrame
 
-    ##################################  3) Kreirati model/e mašinskog učenja  ####################################
-    # Pretvaranje iz Class u label jer tako algoritmi traze
+    plt.figure()
+    sns.heatmap(corr_pd, annot=True, fmt=".2f", cmap="coolwarm", xticklabels=columns_scaled, yticklabels=columns_scaled)
+    plt.title(f"Correlation Heatmap {iter}")
+    
+
+def convert_to_ass(data):
     indexer = StringIndexer(inputCol="Class", outputCol="label")
     indexedData = indexer.fit(data).transform(data)
 
-    # Odabir relevantnih atributa i pretvaranje u vektor
+    # Odabir odgovarajucih kolonaa i pretvaranje u vektor
     assembler = VectorAssembler(inputCols=columns_scaled if PREPROCESS_DATA else columns, outputCol="features")
     assembledData = assembler.transform(indexedData)
+    return assembledData
 
-    # Deljenje podataka na skup za treniranje i skup za testiranje
-    (trainingData, testData) = assembledData.randomSplit([0.8, 0.2], seed=13156123) 
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("Rice Classificator").getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")  # ne zelim da mi se INFO logovi prikazuju u konzoli
+
+
+    ####################################  1) Učitavanje podataka  ##############################################
+    raw_data = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("./modified_riceClassification.csv")
+
+
+    ###############################  2) Preprocesranje? (koristeci spark) #######################################
+    if(PREPROCESS_DATA):
+        assembledData = preprocess_data(raw_data)
+    else:  
+        assembledData = convert_to_ass(raw_data)
+
+    ##############################   3) Kreirati model/e mašinskog učenja  ####################################
+    (trainingData, testData) = assembledData.randomSplit([0.8, 0.2], seed=13156123)
 
     lr = LogisticRegression()
     nb = NaiveBayes()
@@ -140,12 +173,11 @@ if __name__ == "__main__":
     gbt = GBTClassifier()
 
     # Treniranje
-    lrModel = lr.fit(trainingData) 
-    nbModel = nb.fit(trainingData) 
-    svmModel = svm.fit(trainingData) 
+    lrModel = lr.fit(trainingData)
+    nbModel = nb.fit(trainingData)
+    svmModel = svm.fit(trainingData)
     dtModel = dt.fit(trainingData)
     gbtModel = gbt.fit(trainingData)
-
 
     ##################################   4)  Kreirani model/e testirati  #########################################
     lrPredictions = lrModel.transform(testData)
@@ -168,7 +200,7 @@ if __name__ == "__main__":
     gbtAccuracy = gbtEvaluator.evaluate(gbtPredictions, {gbtEvaluator.metricName: "accuracy"})
 
     lrPredictionAndLabels = lrPredictions.select("prediction", "label").rdd
-    classificator_evaluation(lrPredictionAndLabels,"Logistic Regression.txt")
+    classificator_evaluation(lrPredictionAndLabels, "Logistic Regression.txt")
     nbPredictionAndLabels = nbPredictions.select("prediction", "label").rdd
     classificator_evaluation(nbPredictionAndLabels,"Naive Bayes.txt")
     svmPredictionAndLabels = svmPredictions.select("prediction", "label").rdd
@@ -176,7 +208,7 @@ if __name__ == "__main__":
     dtPredictionAndLabels = dtPredictions.select("prediction", "label").rdd
     classificator_evaluation(dtPredictionAndLabels,"Decision Tree.txt")
     gbtPredictionAndLabels = gbtPredictions.select("prediction", "label").rdd
-    classificator_evaluation(gbtPredictionAndLabels,"GBT.txt")                                          
+    classificator_evaluation(gbtPredictionAndLabels,"GBT.txt")
 
     ##################################  5) Sačuvati i prikazati rezultate.  ######################################
     print("Logistic Regression Accuracy: " + str(lrAccuracy))
@@ -185,3 +217,4 @@ if __name__ == "__main__":
     print("Decision Tree Accuracy: " + str(dtAccuracy))
     print("Gradient Boosted Trees: " + str(gbtAccuracy))
     spark.stop()
+    plt.show()
